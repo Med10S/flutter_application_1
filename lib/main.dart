41b749888b6139f3d301d1b89a1813adb93610df
AbdellahTahri <Garb.eco13@gmail.com>
2023-04-17 14:54:53 +0000

    ABDELLAH

--- lib/main.dart
+++ lib/main.dart
@@ -17,57 +17,56 @@ import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((value) => Get.put(AuthentificationRepository()));
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(AuthentificationRepository()));
   //pour changer la couleur le la bar de notification de la même couleur de notre théme
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-       statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
-        statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+      statusBarBrightness: Brightness.light,
       statusBarColor: Color.fromRGBO(47, 103, 23, 1)));
   // WidgetsFlutterBinding.ensureInitialized();
-  
+
   //debugPaintSizeEnabled = true;
-  runApp( 
-     MyApp());
+  runApp(MyApp());
 }
+
 //tesr245
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return 
-       GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        darkTheme: ThemeData(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
           brightness: Brightness.dark,
-        primaryColorDark:const Color.fromARGB(255, 194, 194, 194),
-        primaryColor:Color.fromRGBO(26, 98, 114, 1) ,
-        cardColor:Color.fromRGBO(14, 77, 89, 1) ),
-        //themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-        themeMode: ThemeMode.system,
-        theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xFFE5E5E5),
-          primaryColor: Color.fromRGBO(47, 103, 23, 1),
-          primaryColorDark: Color.fromARGB(255, 245, 245, 245),
-          inputDecorationTheme: InputDecorationTheme(
-            
-              labelStyle: Theme.of(context).textTheme.labelLarge,
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide:
-                      const BorderSide(color: Color.fromRGBO(47, 103, 23, 1))),
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide:
-                      const BorderSide(color: Color.fromRGBO(47, 103, 23, 1))),
-              disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide:
-                      const BorderSide(color: Color.fromRGBO(47, 103, 23, 1)))),
-        ),
-        home: MyScreen(),
-      );
-  ;
+          primaryColorDark: const Color.fromARGB(255, 194, 194, 194),
+          primaryColor: Color.fromRGBO(26, 98, 114, 1),
+          cardColor: Color.fromRGBO(14, 77, 89, 1)),
+      //themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE5E5E5),
+        primaryColor: Color.fromRGBO(47, 103, 23, 1),
+        primaryColorDark: Color.fromARGB(255, 245, 245, 245),
+        inputDecorationTheme: InputDecorationTheme(
+            labelStyle: Theme.of(context).textTheme.labelLarge,
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide:
+                    const BorderSide(color: Color.fromRGBO(47, 103, 23, 1))),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide:
+                    const BorderSide(color: Color.fromRGBO(47, 103, 23, 1))),
+            disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide:
+                    const BorderSide(color: Color.fromRGBO(47, 103, 23, 1)))),
+      ),
+      home: MyScreen(),
+    );
+    ;
   }
 }
