87272e79e9830e0f7f243099e0a5dc177b73236a
Chayma68 <chliyahchaimae2003@gmail.com>
2023-04-22 21:40:04 +0000

    chayma

--- lib/src/adulte_pages/pollution/PollutionAir.dart
+++ lib/src/adulte_pages/pollution/PollutionAir.dart
@@ -13,6 +13,6 @@ class PollutionAir extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(color: Mcolors.couleurPrincipal);
+    throw UnimplementedError();
   }
 }
